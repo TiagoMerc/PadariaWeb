ab29d930f2269693838a36a50b7a6a2f23b7a792
I.Sayuri <isabelasassa@hotmail.com>
2018-09-05 19:35:37 -0300

    identidade visual do site

---
+++ Peter Pão/Panfleto/PanfletoBack/chegar.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html>
+<html lang="pt-BR">
+<meta charset="utf-8">    
+<title>Peter Pão</title>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="panfleto.css" rel="stylesheet">
+    <link href="panfleto.js">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+     <script src="http://maps.google.com/maps/api/js?sensor=false"></script>
+    <link href="css/style.css" rel="stylesheet">
+</head>
+<body>
+<nav id = "nb" class="navbar navbar-default navbar">
+  <div id = "container-navbar" class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+        <a class = "navbar-brand">
+            <h3>Peter Pão <img src="paoIcon.png" class="logo" width="60">
+            </h3>
+        </a>
+    </div>
+   <div class="collapse navbar-collapse" id="myNavbar">
+    <ul class="nav navbar-nav navbar-right">
+        <li><a href="panfleto.php">Home</a></li>
+        <li><a href="historia.php">Nossa História</a></li>
+        <li><a href="produto.php">Nossos Produtos</a></li>
+        <li  class="active"><a href="chegar.php">Como Chegar</a></li>
+        <li><a href="fale.php">Fale Conosco</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<div id="endereco" class=container-fluid>
+        <h4> A Peter Pão possui cinco unidades, prontas para te atender:</h4>
+            <br/>
+            <b>Unidade 1:</b>  Rua J. M. Barrie, 42. Neverland.
+            <br/>
+            <ul class=list>
+                <li> Telefone 1: (15) 7652-8947</li>
+                <li> Telefone 2: (15) 97777-8888</li>
+            </ul>
+        <br/>
+            <b>Unidade 2:</b> Rua The Little White Bird, 12. Disneyland.
+             <br/>
+            <ul class=list>
+                <li>Telefone 1: (15) 1111-2222</li>
+                <li>Telefone 2: (15) 93333-4444</li>
+            </ul>
+        <br/>
+             <b>Unidade 3:</b> Rua The Boy Who Wouldn’t Grow, 132. Pirate Ship.
+             <br/>
+            <ul class=list>
+                <li>Telefone 1: (15) 2121-3947</li>
+                <li>Telefone 2: (15) 93333-5656</li>
+            </ul>
+        <br/>
+             <b>Unidade 4:</b> Avenida Once Upon a Time, 1289. Dreamland.
+             <br/>
+            <ul class=list>
+                <li>Telefone 1: (15) 8981-7895</li>
+                <li>Telefone 2: (15) 90101-5555</li>
+            </ul>
+        <br/>
+             <b>Unidade 5:</b> Kensington Gardens, 901. Orlando.
+            <br/>
+            <ul class=list>
+                <li>Telefone 1: (15) 7777-3433</li>
+                <li>Telefone 2: (15) 91313-5885</li>
+            </ul>
+</div>
+
+
+<!--Footer-->
+<footer class="page-footer">
+    <div id="container-footer" class="container-fluid text-center">
+        <div class="row">
+                <img class=logo src="paoIcon.png" width="60" alt="logo">
+                <p><b>Unidade 1:</b> Rua J. M. Barrie, 42. Neverland.
+                <br/>
+                    <b>Unidade 2:</b> Rua The Little White Bird, 12. Disneyland.
+                <br/>
+                    <b>Unidade 3:</b> Rua The Boy Who Wouldn’t Grow, 132. Pirate Ship.
+                <br/>
+                    <b>Unidade 4:</b> Avenida Once Upon a Time, 1289. Dreamland.
+                <br/>
+                    <b>Unidade 5:</b>  Kensington Gardens, 901. Orlando.
+                </p>
+        </div>
+    </div>
+</footer>
+</body>
+</html>
---
+++ Peter Pão/Panfleto/PanfletoBack/historia.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html>
+<html lang="pt-BR">
+<meta charset="utf-8">
+<title>Peter Pão</title>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="panfleto.css" rel="stylesheet">
+    <link href="panfleto.js">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<link href="css/style.css" rel="stylesheet">
+</head>
+<body>
+
+<nav id = "nb" class="navbar navbar-default navbar">
+  <div id = "container-navbar" class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+        <a class = "navbar-brand">
+            <h3>Peter Pão <img src="paoIcon.png" class="logo" width="60">
+            </h3>
+        </a>
+    </div>
+   <div class="collapse navbar-collapse" id="myNavbar">
+    <ul class="nav navbar-nav navbar-right">
+        <li><a href="panfleto.php">Home</a></li>
+        <li  class="active"><a href="historia.php">Nossa História</a></li>
+        <li><a href="produto.php">Nossos Produtos</a></li>
+        <li><a href="chegar.php">Como Chegar</a></li>
+        <li><a href="fale.php">Fale Conosco</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+
+<div id="chist" class="container">
+  <h1>Nossa História</h1>
+  <p>
+<br/>
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque id lacus sed orci blandit rhoncus. Vestibulum euismod ipsum non vehicula egestas. Sed placerat lacus ligula, vitae faucibus urna convallis eu. Integer finibus sit amet nibh vitae vestibulum. Suspendisse potenti. Nunc porttitor euismod arcu et luctus. Nullam dapibus vestibulum dui a maximus. Cras ac diam eget odio blandit venenatis et a tellus. Nulla facilisi. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+<br/>
+Nam quis ultricies justo, ac gravida enim. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed sit amet libero metus. Maecenas eget sem porta, laoreet quam id, convallis ligula. Proin quis dui et ligula molestie aliquet. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Etiam eget mi nec tortor faucibus venenatis id vel diam.
+<br/>
+Maecenas velit augue, malesuada sit amet ultrices ut, iaculis vitae quam. Aliquam rhoncus sem porttitor libero molestie, quis pharetra diam commodo. Cras porta aliquam mi, vel maximus leo eleifend non. Pellentesque vestibulum leo ac massa ultrices, eget scelerisque neque vestibulum. Integer aliquet in ante in lobortis. Vestibulum nec sodales quam, eget maximus mauris. Praesent ipsum metus, viverra quis eros eu, ultrices eleifend elit. Sed dignissim sapien ligula, a pulvinar leo pellentesque sit amet. Cras lorem lacus, sodales vel turpis vitae, blandit faucibus dui. Morbi magna augue, lobortis ac pulvinar et, hendrerit at quam. Fusce ullamcorper cursus purus non lobortis. Vestibulum et lectus vulputate, convallis nisi quis, semper eros. Vestibulum scelerisque, felis nec maximus consequat, dolor ante porttitor lacus, quis fringilla nunc nisi tempor odio. Integer lobortis, urna at aliquet tincidunt, lacus risus tincidunt nisl, sed porta velit quam id dolor.
+<br/>
+<br/>
+</p>
+    <div class="imagem" >
+        <img src="Imagens/padaria.jpg" width="600" height="345" >
+    </div>
+
+<p>
+<br/>
+<br/>
+Phasellus cursus libero et mauris egestas rutrum. Phasellus id dui a tellus gravida mollis. Curabitur sem sapien, malesuada vitae ultrices nec, iaculis eu lectus. Aenean posuere tincidunt arcu sed sollicitudin. Nullam tellus nisi, pellentesque sed bibendum ac, placerat at purus. Vivamus commodo erat nec orci blandit, vitae dignissim tellus hendrerit. Maecenas dictum in tellus quis volutpat. Nam mollis purus in tellus venenatis, eu finibus orci finibus. Cras vitae ultricies tortor. Nunc sapien quam, mollis nec lacus vitae, faucibus bibendum lorem. Praesent id nisi non felis posuere varius quis et nisl. In hac habitasse platea dictumst. Sed molestie dictum urna, in vestibulum risus auctor sed. Nullam ornare turpis id nibh imperdiet ullamcorper.
+<br/>
+Proin eu finibus enim, vitae bibendum leo. Sed sagittis, sem eget facilisis imperdiet, tortor diam dictum sapien, eu pretium erat turpis vitae enim. Nulla et interdum elit. Donec magna libero, semper ac elit sit amet, mattis interdum lacus. Duis ultricies, erat in aliquet eleifend, mauris magna tincidunt lorem, quis vulputate metus elit et elit. Donec sem ipsum, tincidunt quis leo sit amet, volutpat efficitur justo. Nunc dapibus orci eget turpis pretium hendrerit. Pellentesque id scelerisque mi, a efficitur enim.
+<br/>
+</p>
+
+</div>
+<!--Footer-->
+<footer class="page-footer">
+    <div id="container-footer" class="container-fluid text-center">
+        <div class="row">
+                <img class=logo src="paoIcon.png" width="60" alt="logo">
+                <p><b>Unidade 1:</b> Rua J. M. Barrie, 42. Neverland.
+                <br/>
+                    <b>Unidade 2:</b> Rua The Little White Bird, 12. Disneyland.
+                <br/>
+                    <b>Unidade 3:</b> Rua The Boy Who Wouldn’t Grow, 132. Pirate Ship.
+                <br/>
+                    <b>Unidade 4:</b> Avenida Once Upon a Time, 1289. Dreamland.
+                <br/>
+                    <b>Unidade 5:</b>  Kensington Gardens, 901. Orlando.
+                </p>
+        </div>
+    </div>
+</footer>
+</body>
+
+</html>
